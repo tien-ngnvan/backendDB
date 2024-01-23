cfbe0bbed2a1b88f991e3207d97a6e3b8fb6ad16
Hung Nguyen <hungnq131002@gmail.com>
2024-01-24 00:47:59 +0700

    Revert "refactor: turn to hierarchical structure"
    This reverts commit 033ec8c8acd55301ba2a58c101d8d9d821709b99.

--- src/entity/node/base_node.py
+++ src/entity/node/base_node.py
@@ -345,6 +345,45 @@ class NodeWithScore(BaseModel):
     def get_embedding(self) -> List[float]:
         return self.node.get_embedding()
 
+class ImageNode(TextNode):
+    """Node with image."""
+
+    # TODO: store reference instead of actual image
+    # base64 encoded image str
+    image: Optional[str] = None
+    image_path: Optional[str] = None
+    image_url: Optional[str] = None
+    image_mimetype: Optional[str] = None
+    text_embedding: Optional[List[float]] = Field(
+        default=None,
+        description="Text embedding of image node, if text field is filled out",
+    )
+
+    @classmethod
+    def get_type(cls) -> str:
+        return ObjectType.IMAGE
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "ImageNode"
+
+    def resolve_image(self) -> ImageType:
+        """Resolve an image such that PIL can read it."""
+        if self.image is not None:
+            import base64
+
+            return BytesIO(base64.b64decode(self.image))
+        elif self.image_path is not None:
+            return self.image_path
+        elif self.image_url is not None:
+            # load image from URL
+            import requests
+
+            response = requests.get(self.image_url)
+            return BytesIO(response.content)
+        else:
+            raise ValueError("No image found in node.")
+
 
 # Document Classes for Readers
 
---
+++ src/entity/prompt/__init__.py
@@ -0,0 +1,3 @@
+from .base_prompt import *
+from .mixin import *
+from .prompt_template import *
