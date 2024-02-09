41f007b4056116bad516fd69576d5195a9b65988
Hung Nguyen <hungnq131002@gmail.com>
2024-02-09 21:07:53 +0700

    refactor: change type of some parameter

--- src/config/schema.py
+++ src/config/schema.py
@@ -11,17 +11,17 @@ class MilvusConfig:
     address: str
     uri: str
     user: str
+    embedding_field: str
+    primary_field: str
+    text_field: str
+    consistency_level: str
 
 @dataclass
 class MilvusArguments:
     collection_name: str
-    consistency_level: str
     index_params: Dict[str, Any]
     search_params: Dict[str, Any]
     overwrite: False
-    primary_field: str
-    text_field: str
-    embedding_field: str
     embedding_dim: int
 
 @dataclass
