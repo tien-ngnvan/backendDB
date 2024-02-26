329ec8faf80c9d0d6ae4e5025373a3dcb507684a
Hung Nguyen <hungnq131002@gmail.com>
2024-02-26 23:49:07 +0700

    chore: change way to get absolute path and change function to initialize config

--- examples/test_milvus_db.py
+++ examples/test_milvus_db.py
@@ -1,7 +1,7 @@
-from pathlib import Path
+import os
 import time
 from omegaconf import DictConfig
-from hydra import compose, initialize
+from hydra import compose, initialize_config_dir
 
 from src.pipeline.data_ingestion import InitializeDatabase
 from src.configs.configuration import ConfigurationManager
@@ -11,7 +11,7 @@ import glob
 
 startTime_load = int(round(time.time() * 1000))
 
-pdf_files = glob.glob("/home/hungnq/hungnq_2/backend_db/backendDB/data/*.docx")
+pdf_files = glob.glob(os.path.abspath(os.curdir) + "/data/*.docx")
 print("pdf_files: ", pdf_files)
 reader = DirectoryReader(
     input_files=pdf_files
@@ -24,13 +24,14 @@ if pdf_documents:
 # ------------------
 
 # using hydra to load config
-initialize(version_base=None, config_path="configs/config.yaml")
-cfg: DictConfig = compose("config.yaml", overrides=["db=mysql", "db.user=${oc.env:USER}"])
+initialize_config_dir(version_base=None, config_dir=os.path.abspath(os.curdir) +  "/configs/")
+cfg: DictConfig = compose("config.yaml")
+print(cfg)
 
 # load manager
 manager = ConfigurationManager(config=cfg)
 
-splitrer_config = manager.get_sentence_splitter_config()
+splitter_config = manager.get_sentence_splitter_config()
 milvus_config = manager.get_milvus_config()
 encoder_config = manager.get_cross_embed_config()
 other_config = manager.get_orther_config()
@@ -38,7 +39,7 @@ other_config = manager.get_orther_config()
 
 # cần 2 params and config: của milvus và index -> done
 pipeline = InitializeDatabase(
-    splitrer_config=splitrer_config,
+    splitter_config=splitter_config,
     milvus_config=milvus_config,
     encoder_config=encoder_config,
     other_config=other_config,
